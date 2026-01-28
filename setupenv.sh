2d72f71eef5b19275e5915ae2f23f8a9c523d8c6
James McGloin <jmcgloin@hotmail.com>
2026-01-28 13:42:15 +0000

    Add environment setup

---
+++ setupenv.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -e
+
+PROJECT_REPO="https://github.com/ragabone/provision-ble.git"
+PROJECT_DIR="/opt/provision-ble"
+BUILD_DIR="$PROJECT_DIR/build"
+SERVICE_NAME="provision-ble.service"
+# Install packages
+sudo apt update
+sudo apt install -y \
+  git \
+  cmake \
+  build-essential \
+  pkg-config \
+  libglib2.0-dev \
+  libnm-dev \
+  bluez 
+
+#Setup Bluetooth
+sudo rfkill unblock bluetooth
+sudo bluetoothctl power on
+sudo bluetoothctl discoverable on
+sudo bluetoothctl pairable on
+
+# get project
+sudo git clone "$PROJECT_REPO" "$PROJECT_DIR"
+sudo chown -R "$USER:$USER" "$PROJECT_DIR"
+#build
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+cmake ..
+make -j"$(nproc)"
+#Setup Service
+SERVICE_PATH="/etc/systemd/system/$SERVICE_NAME"
+sudo tee "$SERVICE_PATH" >/dev/null <<EOF
+[Unit]
+Description=Provision BLE Service
+After=bluetooth.service NetworkManager.service
+Requires=bluetooth.service NetworkManager.service
+
+[Service]
+Type=simple
+ExecStart=$BUILD_DIR/provision-ble
+Restart=always
+RestartSec=2
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+#Enable service
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl enable "$SERVICE_NAME"
+sudo systemctl status "$SERVICE_NAME" --no-pager
